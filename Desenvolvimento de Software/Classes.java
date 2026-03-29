423f25ecf5add8bd018d5168204074c0e946eba3
Thurzzgoti <arthuravila440@gmail.com>
2026-03-29 17:29:58 -0300

    atualização do código

--- Desenvolvimento de Software/Classes.java
+++ Desenvolvimento de Software/Classes.java
@@ -4,9 +4,10 @@ public class Classes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Conta conta1 = new Conta();
+        Funcionario func = new Funcionario();
         int opc = 0;
         do{
-            System.out.println("----SISTEMA BANCÁRIO----\n1- Cadastrar conta\n2- Ver dados cadastrados\n3-Fazer saque\n4- Fazer depósito\n5- Sair");
+            System.out.println("----SISTEMA BANCÁRIO----\n1- Cadastrar conta\n2- Ver dados cadastrados\n3-Fazer saque\n4- Fazer depósito\n5- Cadastrar funcionário\n6- Ver funcionário cadastrado\n7- Aplicar bonificação para o funcionário\n8- Sair");
             System.out.println("Digite a opção desejada");
             opc = sc.nextInt();
             switch (opc) {
@@ -43,8 +44,39 @@ public class Classes {
                     conta1.Deposito(valor1);
                     System.out.println("Novo saldo: "+ conta1.getSaldo());
                     break;
+                case 5:
+                    sc.nextLine();
+                    System.out.println("Digite o nome do funcionário");
+                    String fn = sc.nextLine();
+                    func.setNome(fn);
+                    System.out.println("Digite o departamento do funcionário");
+                    String dp = sc.nextLine();
+                    func.setDepartamento(dp);
+                    System.out.println("Digite o RG do funcionário");
+                    String reg = sc.nextLine();
+                    func.setRg(reg);
+                    System.out.println("Digite o salário do funcionário");
+                    double salar = sc.nextDouble();
+                    func.setSalario(salar);
+                    System.out.println("Digite a data de contratação do funcionário");
+                    sc.nextLine();
+                    String dat = sc.nextLine();
+                    func.setCont(dat);
+                    System.out.println("O funcionário ainda está ativo?");
+                    String at = sc.nextLine();
+                    func.setAtivo(at);
+                    break;
+                case 6:
+                    System.out.println("----DADOS DO FUNCIONÁRIO----:\nNome: "+ func.getNome() + "\nDepartamento: "+ func.getDepartamento() + "\nRG: "+ func.getRg() + "\nSalário: " + func.getSalario() + "\nData de Contratação: "+ func.getCont() + "\nFuncionário ativo: " + func.getAtivo());       
+                    break;
+                case 7:
+                    System.out.println("Quanto será a bonificação do funcinário?");
+                    double boni = sc.nextDouble();
+                    func.bonifica(boni);
+                    System.out.println("Bonificação aplicada!\nSalário atual: " + func.getSalario());
+                    break;
             }
-        } while(opc != 5);
+        } while(opc != 8);
         sc.close();
         
     }
